8895254dae1f5295dc6a876fd927ccd5057ccb39
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-04 12:58:27 +0530

    router set

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const router = createBroswerRouter([
     }
 ])
 root.render(
-    <App />
+    <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
