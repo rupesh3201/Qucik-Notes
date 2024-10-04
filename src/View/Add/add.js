a4bbf50c56210bc06b6ce27f0c433188485ca01c
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-04 12:56:07 +0530

    Created folder structure

---
+++ src/View/Add/add.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './add.css'
+function add() {
+  return (
+    <div>add</div>
+  )
+}
+
+export default add
---
+++ src/View/Show/show.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './show.css'
+function show() {
+  return (
+    <div>show</div>
+  )
+}
+
+export default show
