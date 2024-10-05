17bf4b35d025fd93b69b20bb0d333aa81403c83f
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-05 21:06:48 +0530

    1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
 import Home from './View/Home/home';
 import Add from './View/Add/add'
 import Show from './View/Show/show'
@@ -21,11 +22,19 @@ const router = createBrowserRouter([
     {
         path: "/show",
         element: <Show/>
+    }, {
+        path: "*",
+        element: <h1 className='not'>404 Not Found</h1>
     }
 ])
 root.render(
+    <div>
+    
     <RouterProvider router={router}/>
+    <Toaster/>
+    </div>
 );
+//  1 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
