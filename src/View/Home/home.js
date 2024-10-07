c062607c062247ea468dfca89761cfe7161229be
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-07 20:13:18 +0530

    Align center

--- src/View/Home/home.js
+++ src/View/Home/home.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './home.css'
 function Home() {
   return (
-    <div className='body'>home</div>
+    <div>
+      <h1 className='Heading'>Qucik Notes 📝 </h1>
+    </div>
   )
 }
 
