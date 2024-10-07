78fedfdfd00c4b82a9f38e20f3d59118a086eabd
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-07 20:02:25 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ root.render(
     <Toaster/>
     </div>
 );
-//  1 
+//  3
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
