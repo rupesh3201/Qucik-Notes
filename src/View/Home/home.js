0967a6a79edec4fe79b603dc72535e282b8d6188
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-07 20:22:46 +0530

    Align center

--- src/View/Home/home.js
+++ src/View/Home/home.js
@@ -4,6 +4,8 @@ function Home() {
   return (
     <div>
       <h1 className='Heading'>Qucik Notes 📝 </h1>
+      <h5 className='Heading'><span>e</span>at <span>c</span>ode <span>s</span>leep <span>r</span>epeat</h5>
+      <h4 className='Heading'>“Start where you are. Use what you have. Do what you can"</h4>
     </div>
   )
 }
