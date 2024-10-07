822676ef8eeea703d5a483cc703f9a89a45c9825
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-08 01:59:41 +0530

    Added ui

--- src/View/Home/home.js
+++ src/View/Home/home.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import './home.css'
+import shield from '../../Assets/shield (1).png'
+import easy from '../../Assets/easy.png'
+import Stress from '../../Assets/stress.png'
 function Home() {
   return (
     <div>
     <div>
-      <h1 className='Heading'> <span>Q</span>ucik <span>N</span>otes 📝 </h1>
-      <h4 className='Heading'>“ Start where you are. Use what you have. Do what you can🥷🏻” </h4>
+      <h1 className='Heading'> <span></span>Qucik <span></span>Notes 📝 </h1>
+      <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4>
     </div>
     <div>
       <h3 className='whyquciknotes'> Why Quick Notes ? </h3>
+      <div className='Feaures_container'>
+      <div className='Features_card'>
+        <img src={shield} className='Features_Logo'></img>
+        <p className='Features_Dispt'>notes will be securely stored in browser local storage </p>
+      </div>
+      <div className='Features_card'>
+        <img src={easy} className='Features_Logo'></img>
+        <p className='Features_Dispt'>easy to use, life-time access </p>
+      </div>
+      <div className='Features_card'>
+        <img src={Stress} className='Features_Logo'></img>
+        <p className='Features_Dispt'>use your brain to think, not to remember </p>
+      </div>
+     
+      </div>
     </div>
     <div className='Button_container'>
       < button className='action_btn' > Add Notes ✏️ </button>
       < button className='action_btn' > Views Notes📄</button>
-
-      
-
     </div>
     </div>
+ 
   )
 }
 
