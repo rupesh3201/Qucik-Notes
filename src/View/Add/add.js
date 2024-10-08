9b331a2d0e549743607f1fcc2a0a72c50d13eb6b
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-09 01:31:22 +0530

    UsetState Imported

--- src/View/Add/add.js
+++ src/View/Add/add.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './add.css'
 import HomeBtn from './../../Component/Home/HomeBtn'
 
 function Add() {
+  const [Title , setTitle] = useState("")
   return (
     <div>
     <div>
       <h1 className='Heading'> <span></span>Add <span></span>Notes ✏️ </h1>
       {/* <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4> */}
     </div>
+    <input type='text ' placeholder='Title'/>
    <HomeBtn/>
-
     </div>
   )
 }
