bfbc76b506ad1539d33aebe629480cd1111d29d6
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-08 21:10:34 +0530

    component created

--- src/View/Add/add.js
+++ src/View/Add/add.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './add.css'
+import ImgHome from './../../Assets/home.png'
+import { Link } from 'react-router-dom'
 function Add() {
   return (
     <div>
-      <h1 className='Heading'> <span></span>Add <span></span>Notes 📝 </h1>
-      <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4>
+    <div>
+      <h1 className='Heading'> <span></span>Add <span></span>Notes ✏️ </h1>
+      {/* <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4> */}
+    </div>
+    <div>
+     <Link to={"/"}>
+      <img src={ImgHome} className='Actiontbtn-back' alt=''></img>
+      </Link>
+    </div>
+
     </div>
   )
 }
