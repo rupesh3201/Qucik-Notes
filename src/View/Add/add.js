0c5804b86b7a19e31d58ca17357283b9839acc39
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-08 21:01:58 +0530

    add notes title added

--- src/View/Add/add.js
+++ src/View/Add/add.js
@@ -2,7 +2,10 @@ import React from 'react'
 import './add.css'
 function Add() {
   return (
-    <div>add</div>
+    <div>
+      <h1 className='Heading'> <span></span>Add <span></span>Notes 📝 </h1>
+      <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4>
+    </div>
   )
 }
 
