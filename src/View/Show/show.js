b9c3071e4c6721f9715b6ab718e2d0f115c39546
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-09 01:26:31 +0530

    Added home button

--- src/View/Show/show.js
+++ src/View/Show/show.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './show.css'
+import HomeBtn from './../../Component/Home/HomeBtn'
 function Show() {
   return (
     <div>
@@ -7,6 +8,7 @@ function Show() {
       <h1 className='Heading'> <span></span>View <span></span>Notes 📄 </h1>
       {/* <h4 className='SubHeading'>“ Start where<span>  you </span>are. Use what <span> you  </span> have.  Do what<span>  you  </span>can🥷🏻” </h4> */}
     </div>
+    <HomeBtn/>
     </div>
   )
 }
