207d44099b1021e008c86cea53af558feec0bc48
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-14 23:35:06 +0530

    Added CSS

--- src/Component/NoteCard/Notecard.js
+++ src/Component/NoteCard/Notecard.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import  './Notecard.css'
-function Notecard({Title, Discription , Category , Emoji }) {
+import React from 'react';
+import './Notecard.css';
+
+function Notecard({ Title, Description, Category, Emoji }) {  // Changed Discription to Description
   return (
     <div className='NotecardConatiner'>
-        <div className='Notecard'>
-        <h1 className='Notecard_emoji'> {Emoji}</h1>
-         <h1> {Title}</h1>
-       </div>
-
+      <div className='Notecard'>
+        <h1 className='Notecard_emoji'>{Emoji}</h1>
+        <div className='NotecardDis'>
+          <h1>{Title}</h1>
+          <p>{Category}</p>
+          <p>{Description}</p> {/* Fixed the prop name */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Notecard
+export default Notecard;
