c99929679a8b07f642bce610f640ab05e590a7d7
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-14 23:13:43 +0530

    css

--- src/Component/NoteCard/Notecard.js
+++ src/Component/NoteCard/Notecard.js
@@ -2,14 +2,10 @@ import React from 'react'
 import  './Notecard.css'
 function Notecard({Title, Discription , Category , Emoji }) {
   return (
-    <div>
-        <div className='CardConatiner'>
-       <div className='Card_Logo'>
-            {Emoji}
-       </div>
-       <div className='Crad_Title'>
-           <h3> {Title}</h3>
-       </div>
+    <div className='NotecardConatiner'>
+        <div className='Notecard'>
+        <h1 className='Notecard_emoji'> {Emoji}</h1>
+         <h1> {Title}</h1>
        </div>
 
     </div>
